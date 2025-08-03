a21cba1a82afe8e3ce921aba1d3306d0874e8d67
Etay Schwartzberg <etayschwartzberg@Etays-MacBook-Pro.local>
2025-08-02 22:03:35 -0500

    users db model

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from app.db import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
