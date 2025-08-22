8a3577641d660ccd7aec8ded11b0c6e6d6996cb7
Etay Schwartzberg <etay.schwartzberg@gmail.com>
2025-08-22 15:33:27 -0500

    test balance

---
+++ tests/unit/services/test_balance_service.py
@@ -0,0 +1,15 @@
+import pytest
+from app.services.balance_service import check_balance
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("income, expenses, expected_result", [
+    (1000, 500, 500),
+    (500, 1000, -500),
+    (1000, 1000, 0),
+    (0, 0, 0),
+    (100, 0, 100),
+    (0, 100, -100)
+])
+async def test_check_balance(income, expenses, expected_result):
+    response = await check_balance(income, expenses)
+    assert response == expected_result
