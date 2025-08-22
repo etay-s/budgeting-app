eae5ec15d44eb83e44ddda1cc153de2b4aa2abb7
etay-s <69721225+etay-s@users.noreply.github.com>
2025-08-22 16:02:23 -0500

    Setup Unit Testing & Actions & Pre-Push Hook
    * pytest deps
    
    * config and smoke test
    
    * fix for running tests
    
    * test balance
    
    * remove sanity test
    
    * test structure and name
    
    * GitHub Action for testing
    
    * pre-commit hook to run tests before push
    
    * lint
    
    ---------
    
    Co-authored-by: Etay Schwartzberg <etay.schwartzberg@gmail.com>

---
+++ tests/unit/services/test_balance_service.py
@@ -0,0 +1,20 @@
+import pytest
+from app.services.balance_service import check_balance
+
+
+class TestBalanceService:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "income, expenses, expected_result",
+        [
+            (1000, 500, 500),
+            (500, 1000, -500),
+            (1000, 1000, 0),
+            (0, 0, 0),
+            (100, 0, 100),
+            (0, 100, -100),
+        ],
+    )
+    async def test_returns_correct_balance(self, income, expenses, expected_result):
+        response = await check_balance(income, expenses)
+        assert response == expected_result
