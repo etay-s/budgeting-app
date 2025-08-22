ba951149f4930baa4c28a6316db03087e54e5e06
Etay Schwartzberg <etay.schwartzberg@gmail.com>
2025-08-22 16:41:00 -0500

    int to NonNegativeFloat for amounts

--- app/schemas/balance.py
+++ app/schemas/balance.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, NonNegativeFloat
 
 
 class BalanceRequest(BaseModel):
-    income: int
-    expenses: int
+    income: NonNegativeFloat
+    expenses: NonNegativeFloat
 
 
 class BalanceResponse(BaseModel):
-    balance: int
+    balance: NonNegativeFloat
