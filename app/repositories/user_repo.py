6eef628a8e12f99366b43f246d513976a18200e0
Etay Schwartzberg <etay.schwartzberg@gmail.com>
2025-08-25 14:52:17 -0500

    encapsulate db logic in repo

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -1,10 +1,23 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from app.db import AsyncSessionLocal
 from app.models import User
 
 
-# TODO: Error handling
-async def get_user_by_email(session: AsyncSession, email: str) -> User | None:
-    stmt = select(User).where(User.email == email)
-    result = await session.execute(stmt)
-    return result.scalar_one_or_none()
+async def create_user(user: User) -> User:
+    async with AsyncSessionLocal() as session:
+        try:
+            session.add(user)
+            await session.commit()
+            await session.refresh(user)
+            return user
+        except IntegrityError:
+            await session.rollback()
+            raise
+
+async def get_user_by_email(email: str) -> User | None:
+    async with AsyncSessionLocal() as session:
+        stmt = select(User).where(User.email == email)
+        result = await session.execute(stmt)
+        return result.scalar_one_or_none()
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -4,28 +4,25 @@ from app.models import User
 from app.auth import hash_password, verify_password, create_access_token
 from app.repositories.user_repo import get_user_by_email
 from sqlalchemy.exc import IntegrityError
+from app.repositories.user_repo import create_user, get_user_by_email
 
 
 async def register_user(name: str, email: str, password: str) -> str:
     async with AsyncSessionLocal() as session:
         try:
             user = User(name=name, email=email, hashed_password=hash_password(password))
-            session.add(user)
-            await session.commit()
-            await session.refresh(user)
+            create_user(user)
             return user.id
         except IntegrityError as e:
-            await session.rollback()
             raise ValueError("Email already registered") from e
 
 
 async def login_user(email: str, password: str) -> str:
-    async with AsyncSessionLocal() as session:
-        user = await get_user_by_email(session, email)
+    user = await get_user_by_email(email)
 
-        if not user or not verify_password(password, user.hashed_password):
-            raise ValueError("Invalid credentials")
+    if not user or not verify_password(password, user.hashed_password):
+        raise ValueError("Invalid credentials")
 
-        token = create_access_token({"sub": str(user.id), "email": user.email})
+    token = create_access_token({"sub": str(user.id), "email": user.email})
 
-        return token
+    return token
