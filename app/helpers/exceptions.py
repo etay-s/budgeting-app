7913c9729a8528b914ba8506de990be5456984c9
Etay Schwartzberg <etay.schwartzberg@gmail.com>
2025-08-25 15:39:39 -0500

    Custom Exceptions

---
+++ app/helpers/exceptions.py
@@ -0,0 +1,25 @@
+from app.routes.utils.http_status_codes import HTTPStatusCodes
+
+class AppError(Exception):
+    http_status_code = HTTPStatusCodes.BAD_REQUEST
+    title = "Bad Request"
+
+    def __init__(self, message: str = "Request could not be processed"):
+        super().__init__(self.message)
+        self.message = message
+
+class UnauthorizedError(AppError):
+    http_status_code = HTTPStatusCodes.UNAUTHORIZED
+    title = "Unauthorized"
+
+class PermissionDeniedError(AppError):
+    http_status_code = HTTPStatusCodes.FORBIDDEN
+    title = "Permission Denied"
+
+class NotFound(AppError):
+    http_status_code = HTTPStatusCodes.NOT_FOUND
+    title = "Not Found"
+
+class AlreadyExist(AppError):
+    http_status_code = HTTPStatusCodes.CONFLICT
+    title = "Already Exist"
